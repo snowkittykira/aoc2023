efe227c416b8f7d08da0f88a92afe28361b061aa
Kira Boom <snowkittykira@gmail.com>
2023-12-01 21:21:09 -0500

    shorten ts and rs versions of 1-2

--- rust/src/bin/01-2.rs
+++ rust/src/bin/01-2.rs
@@ -12,24 +12,17 @@ fn main() -> io::Result<()> {
 
     let mut total = 0;
 
-    let numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
-    let word_numbers = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+    let numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9",
+                   "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
     for line in input.lines() {
         let mut first = None;
         let mut last = None;
         for (i, _) in line.char_indices() {
-
             for (j, num_str) in numbers.iter().enumerate() {
                 if line[i..].starts_with(num_str) {
-                    first = first.or(Some(j + 1));
-                    last = Some(j + 1);
-                }
-            }
-
-            for (j, num_str) in word_numbers.iter().enumerate() {
-                if line[i..].starts_with(num_str) {
-                    first = first.or(Some(j + 1));
-                    last = Some(j + 1);
+                    let num = j%9 + 1;
+                    first = first.or(Some(num));
+                    last = Some(num);
                 }
             }
         }
--- typescript/src/01-2.ts
+++ typescript/src/01-2.ts
@@ -4,35 +4,26 @@ const input = readFileSync ('../data/01.txt', 'utf-8')
 
 let total = 0;
 
-let numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
-let wordNumbers = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+let numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9',
+               'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
 
 input.split('\n').forEach(line => {
     let first: number | null = null;
     let last: number | null = null;
-    console.log(line);
 
     for (let i = 0; i < line.length; i++) {
         const str = line.substring(i);
         numbers.forEach ((n, j) => {
             if (str.startsWith(n)) {
-                first = first || (j+1)
-                last = j+1
+                let num = j%9 + 1
+                first = first || num;
+                last = num;
             }
         })
-        wordNumbers.forEach ((n, j) => {
-            if (str.startsWith(n)) {
-                first = first || j+1
-                last = j+1
-            }
-        })
-
     }
 
     if (first !== null && last !== null) {
         const lineNum = first * 10 + last;
-        console.log(lineNum);
-
         total += lineNum;
     }
 })
